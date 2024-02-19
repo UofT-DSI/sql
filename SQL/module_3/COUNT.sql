c590e868c806c6c6ab030133ef0578c71030e6cd
CODE, POTATO <t.rose.github@protonmail.com>
2024-02-19 14:21:03 -0500

    removing SQLs from last class

--- SQL/module_3/COUNT.sql
+++
@@ -1,9 +0,0 @@
---COUNT
-
--- SELECT COUNT(*) AS rows
--- FROM customer_purchases
-
-SELECT market_date, count(vendor_id) as sales_made, count(DISTINCT customer_id) as number_of_custs
-FROM customer_purchases
-GROUP BY market_date
-ORDER BY market_date
