d1345cd2a46422b212114d17befae4c764544253
Anjali Deshpande <anju@users.noreply.github.com>
2024-05-16 15:32:19 -0400

    Corrected spellig error

--- 03_homework/homework_2.sql
+++ 03_homework/homework_2.sql
@@ -35,7 +35,7 @@ columns and add a column called prod_qty_type_condensed that displays the word �
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 SELECT product_id, product_name,
 CASE WHEN product_qty_type = 'unit' THEN 'unit' ELSE 'bulk' END
-	   AS prod_qty_type_condense
+	   AS prod_qty_type_condensed
 FROM product
 
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
@@ -43,7 +43,7 @@ add a column to the previous query called pepper_flag that outputs a 1 if the pr
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 SELECT product_id, product_name,
 CASE WHEN product_qty_type = 'unit' THEN 'unit' ELSE 'bulk' END
-	   AS prod_qty_type_condense,
+	   AS prod_qty_type_condensed,
 CASE WHEN product_name LIKE '%pepper%' THEN 1 ELSE 0 END 
 	   AS pepper_flag
 FROM product
