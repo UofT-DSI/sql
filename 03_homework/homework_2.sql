eed067f281ba698807955c4ae4f1165646b57c6e
Tristan Liu <nelsonlsy@hotmail.com>
2024-05-17 19:33:25 -0400

    resubmit of homework2 but the content will be duplicate in the file this is just for the purpose of the answers to be seen in the pull request

--- 03_homework/homework_2.sql
+++ 03_homework/homework_2.sql
@@ -2,15 +2,19 @@
 /* 1. Write a query that returns everything in the customer table. */
 
 select * from customer
+select * from customer
+
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
 
 select * from customer order by "customer_last_name", "customer_first_name" limit 10
+select * from customer order by "customer_last_name", "customer_first_name" limit 10
 
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
+select * from customer_purchase where product_id in (4,9)
 select * from customer_purchase where product_id in (4,9)
 -- option 1
 		
@@ -26,15 +30,25 @@ filtered by vendor IDs between 8 and 10 (inclusive) using either:
 */
 -- option 1
 SELECT *,(quantity * cost_to_customer_per_qty) AS price FROM customer_purchases WHERE vendor_id >= 8 and vendor_id<= 10
-
+SELECT *,(quantity * cost_to_customer_per_qty) AS price FROM customer_purchases WHERE vendor_id >= 8 and vendor_id<= 10
 -- option 2
 SELECT *,(quantity * cost_to_customer_per_qty) AS price FROM customer_purchases WHERE vendor_id BETWEEN 8 AND 10
-
+SELECT *,(quantity * cost_to_customer_per_qty) AS price FROM customer_purchases WHERE vendor_id BETWEEN 8 AND 10
 --CASE
 /* 1. Products can be sold by the individual unit or by bulk measures like lbs. or oz. 
 Using the product table, write a query that outputs the product_id and product_name
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
+SELECT 
+    product_id, 
+    product_name,
+    CASE 
+        WHEN product_qty_type = 'unit' THEN 'unit'
+        ELSE 'bulk'
+    END AS prod_qty_type_condensed
+FROM 
+    product;
+
 SELECT 
     product_id, 
     product_name,
@@ -62,9 +76,35 @@ SELECT
 FROM 
     product;
 
+SELECT 
+    product_id, 
+    product_name,
+	CASE 
+        WHEN product_qty_type = 'unit' THEN 'unit'
+        ELSE 'bulk'
+    END AS prod_qty_type_condensed,
+    CASE 
+        WHEN LOWER(product_name) LIKE '%pepper%' THEN 1
+        ELSE 0
+    END AS pepper_flag
+FROM 
+    product;
+
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
+SELECT 
+    ven.vendor_id,
+    ven.vendor_name,
+    vba.market_date,
+FROM 
+    vendor AS ven
+INNER JOIN 
+    vendor_booth_assignments AS vba ON ven.vendor_id = vba.vendor_id
+ORDER BY 
+    ven.vendor_name, 
+    vba.market_date;
+
 SELECT 
     ven.vendor_id,
     ven.vendor_name,
