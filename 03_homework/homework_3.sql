346b3640cbd9c13d3df9444787eb230c473512ef
Ginny1209 <61629844+Ginny1209@users.noreply.github.com>
2024-05-17 16:42:41 -0400

    Update homework_3.sql
    comit from homework

--- 03_homework/homework_3.sql
+++ 03_homework/homework_3.sql
@@ -1,7 +1,9 @@
 -- AGGREGATE
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
-
+SELECT vendor_id, COUNT(*) AS rental_count
+FROM vendor_booth_assignments
+GROUP BY vendor_id;
 
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
@@ -9,7 +11,21 @@ sticker to everyone who has ever spent more than $2000 at the market. Write a qu
 of customers for them to give stickers to, sorted by last name, then first name. 
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
-
+SELECT 
+    c.customer_id, 
+    c.customer_first_name, 
+    c.customer_last_name,
+    SUM(cp.quantity * cp.cost_to_customer_per_qty) AS sumspending
+FROM 
+    customer c
+INNER JOIN 
+    customer_purchases cp ON c.customer_id = cp.customer_id
+GROUP BY 
+    c.customer_id, c.customer_first_name, c.customer_last_name
+HAVING 
+    sumspending > 2000
+ORDER BY 
+    c.customer_last_name, c.customer_first_name;
 
 
 --Temp Table
@@ -20,9 +36,13 @@ HINT: This is two total queries -- first create the table from the original, the
 When inserting the new vendor, you need to appropriately align the columns to be inserted 
 (there are five columns to be inserted, I've given you the details, but not the syntax) 
 
+CREATE TABLE temp.new_vendor AS
+SELECT * FROM vendor;
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
+INSERT INTO temp.new_vendor (vendor_id, vendor_name, vendor_owner_first_name, vendor_owner_last_name)
+VALUES (10, 'Thomass Superfood Store', 'Thomas', 'Rosenthal');
 
 
 
@@ -31,10 +51,24 @@ VALUES(col1,col2,col3,col4,col5)
 
 HINT: you might need to search for strfrtime modifers sqlite on the web to know what the modifers for month 
 and year are! */
-
+SELECT 
+    customer_id, 
+    strftime('%m', market_date) AS month, 
+    strftime('%Y', market_date) AS year
+FROM 
+    customer_purchases;
 /* 2. Using the previous query as a base, determine how much money each customer spent in April 2019. 
 Remember that money spent is quantity*cost_to_customer_per_qty. 
 
 HINTS: you will need to AGGREGATE, GROUP BY, and filter...
 but remember, STRFTIME returns a STRING for your WHERE statement!! */
-
+SELECT 
+    customer_id, 
+    SUM(quantity * cost_to_customer_per_qty) AS totalspending
+FROM 
+    customer_purchases
+WHERE 
+    strftime('%Y', market_date) = '2019' 
+    AND strftime('%m', market_date) = '04'
+GROUP BY 
+    customer_id;
