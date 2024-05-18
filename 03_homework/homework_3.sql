ac7d26fb727511c1cfaab6156fa211ba1a86c7ee
Catherine Zhu <102681357+CatherineZYH@users.noreply.github.com>
2024-05-18 17:39:36 -0400

    Update homework_3.sql

--- 03_homework/homework_3.sql
+++ 03_homework/homework_3.sql
@@ -2,14 +2,25 @@
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
 
-
+select vendor_id, count(vendor_id)as booth_rental_count
+from vendor_booth_assignments
+group by vendor_id
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
 sticker to everyone who has ever spent more than $2000 at the market. Write a query that generates a list 
 of customers for them to give stickers to, sorted by last name, then first name. 
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
-
+select c.customer_id,
+c.customer_first_name,
+c.customer_last_name,
+sum(quantity*cost_to_customer_per_qty)as spend
+from customer c
+inner join customer_purchases cp
+	on c.customer_id=cp.customer_id
+GROUP BY c.customer_id, c.customer_last_name, c.customer_first_name
+HAVING sum(quantity*cost_to_customer_per_qty)> 2000
+ORDER BY c.customer_last_name,c. customer_first_name
 
 
 --Temp Table
