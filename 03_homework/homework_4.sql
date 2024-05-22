e8e821c7bd53a337b4e46287edea3237f6ccc90e
Shiraz Latif <engr_shiraz@hotmail.com>
2024-05-22 00:41:12 -0400

    Homework-4 Submission

--- 03_homework/homework_4.sql
+++ 03_homework/homework_4.sql
@@ -3,11 +3,14 @@
 We tell them, no problem! We can produce a list with all of the appropriate details. 
 
 Using the following syntax you create our super cool and not at all needy manager a list:
+*/
 
 SELECT 
 product_name || ', ' || product_size|| ' (' || product_qty_type || ')'
 FROM product
 
+
+/*
 But wait! The product table has some bad data (a few NULL values). 
 Find the NULLs and then using COALESCE, replace the NULL with a 
 blank for the first problem, and 'unit' for the second problem. 
@@ -15,9 +18,12 @@ blank for the first problem, and 'unit' for the second problem.
 HINT: keep the syntax the same, but edited the correct components with the string. 
 The `||` values concatenate the columns into strings. 
 Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
-All the other rows will remain the same.) */
+All the other rows will remain the same.) 
+*/
 
-
+SELECT 
+product_name || ', ' || coalesce(product_size,' ') || ' (' || coalesce(product_qty_type, 'unit') || ')'
+FROM product
 
 
 --Windowed Functions
@@ -30,11 +36,27 @@ each new market date for each customer, or select only the unique market dates p
 (without purchase details) and number those visits. 
 HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
 
+SELECT *
+	, row_number() OVER (PARTITION by customer_id ORDER by market_date) as visit_num
+	from customer_purchases
 
 /* 2. Reverse the numbering of the query from a part so each customer’s most recent visit is labeled 1, 
 then write another query that uses this one as a subquery (or temp table) and filters the results to 
 only the customer’s most recent visit. */
 
+select * from (
+	SELECT *
+		, row_number() OVER (PARTITION by customer_id ORDER by market_date DESC) as visit_num 
+		from customer_purchases
+	) 
+	where visit_num = 1 -- Most recent visit 
+
+	
 
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
+
+
+SELECT *
+	, count() OVER (PARTITION by customer_id, product_id) as same_prod_purchase_cnt
+	from customer_purchases
