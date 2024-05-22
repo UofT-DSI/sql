376b95f0cc0e1a65787eda6d3eff758366a24a8f
Veronika Plonsak <plensakveronika@gmail.com>
2024-05-21 23:38:38 -0400

    I update homework

--- 03_homework/homework_3.sql
+++ 03_homework/homework_3.sql
@@ -2,7 +2,12 @@
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
 
+-- AGGREGATE 1: How many times each vendor has rented a booth at the farmer’s market 
+--by counting the vendor booth assignments per vendor_id.
 
+SELECT vendor_id, COUNT(*) AS booth_count
+FROM vendor_booth_assignments
+GROUP BY vendor_id;
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
 sticker to everyone who has ever spent more than $2000 at the market. Write a query that generates a list 
@@ -10,7 +15,14 @@ of customers for them to give stickers to, sorted by last name, then first name.
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
 
-
+-- AGGREGATE 2: List of customers who spent more than $2000
+SELECT c.customer_id, c.customer_last_name, c.customer_first_name
+FROM customer c 
+JOIN customer_purchases cp ON c.customer_id = cp.customer_id
+GROUP BY c.customer_id, c.customer_last_name, c.customer_first_name
+HAVING SUM(cp.quantity * cp.cost_to_customer_per_qty) > 2000
+ORDER BY c.customer_last_name, c.customer_first_name;
+DROP TABLE IF EXISTS temp_new_vendor;
 
 --Temp Table
 /* 1. Insert the original vendor table into a temp.new_vendor and then add a 10th vendor: 
@@ -23,7 +35,11 @@ When inserting the new vendor, you need to appropriately align the columns to be
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
-
+-- Temp Table: Inserting the original vendor table into a temp table and inserting a new 10th vendor
+CREATE TEMP TABLE temp_new_vendor AS
+SELECT * FROM vendor;
+INSERT INTO temp_new_vendor (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+VALUES (10, 'Thomass Superfood Store', 'Fresh Focused', 'Thomas', 'Rosenthal');
 
 
 -- Date
@@ -37,4 +53,16 @@ Remember that money spent is quantity*cost_to_customer_per_qty.
 
 HINTS: you will need to AGGREGATE, GROUP BY, and filter...
 but remember, STRFTIME returns a STRING for your WHERE statement!! */
+
+---- Date 1: Get customer_id, month, and year of every purchase
+SELECT customer_id,
+       STRFTIME('%m', transaction_time) AS month, STRFTIME('%Y', transaction_time) AS year
+FROM customer_purchases;
+
+
+-- Date 2: How much money spent by each customer in April 2019
+SELECT customer_id, SUM(quantity * cost_to_customer_per_qty) AS total_spent
+FROM customer_purchases
+WHERE STRFTIME('%m', transaction_time) = '04' AND STRFTIME('%Y', transaction_time) = '2019'
+GROUP BY customer_id;
 
