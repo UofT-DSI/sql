19b741b85ae901cf2543660e5cafdc69972eab47
Viv Yuen <78055035+vivyuen@users.noreply.github.com>
2024-09-17 20:45:24 -0400

    Update homework_2.sql

--- 02_activities/homework/homework_2.sql
+++ 02_activities/homework/homework_2.sql
@@ -1,17 +1,24 @@
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
 
-
+SELECT  customer_id, customer_first_name, customer_last_name, customer_postal_code
+FROM customer;
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
 
-
-
+SELECT  customer_id, customer_first_name, customer_last_name, customer_postal_code
+FROM customer
+ORDER BY customer_last_name, customer_first_name
+LIMIT 10;
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
 -- option 1
+
+SELECT product_id, customer_id
+FROM customer_purchases
+WHERE product_id in (4,9);
 
 -- option 2
 
@@ -22,8 +29,17 @@ filtered by vendor IDs between 8 and 10 (inclusive) using either:
 */
 -- option 1
 
+SELECT vendor_id,  product_id, customer_id,
+(quantity*cost_to_customer_per_qty) AS 'price'
+FROM customer_purchases
+WHERE vendor_id >= 8 AND vendor_id <= 10;
 
 -- option 2
+
+SELECT vendor_id, product_id, customer_id,
+(quantity*cost_to_customer_per_qty) AS 'price'
+FROM customer_purchases
+WHERE vendor_id BETWEEN 8 AND 10;
 
 --CASE
 /* 1. Products can be sold by the individual unit or by bulk measures like lbs. or oz. 
@@ -31,12 +47,117 @@ Using the product table, write a query that outputs the product_id and product_n
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 
+SELECT product_id, product_name
+, CASE WHEN product_qty_type = 'unit'
+	THEN 'unit' 
+	ELSE 'bulk'
+	END AS prod_qty_type_condensed
+	
+FROM product;
 
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 
+SELECT product_id, product_name
+, CASE WHEN product_qty_type = 'unit'
+	THEN 'unit' 
+	ELSE 'bulk'
+	END AS prod_qty_type_condensed
+, CASE WHEN LOWER(product_name) LIKE LOWER('%pepper%' )
+	THEN 1
+	ELSE 0
+	END AS pepper_flag
+	
+FROM product;
 
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
+
+SELECT v.*, vba.*
+FROM vendor as v
+JOIN vendor_booth_assignments AS vba
+ON v.vendor_id = vba.vendor_id
+ORDER BY vendor_name, market_date--SELECT
+/* 1. Write a query that returns everything in the customer table. */
+
+SELECT  customer_id, customer_first_name, customer_last_name, customer_postal_code
+FROM customer;
+
+/* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
+sorted by customer_last_name, then customer_first_ name. */
+
+SELECT  customer_id, customer_first_name, customer_last_name, customer_postal_code
+FROM customer
+ORDER BY customer_last_name, customer_first_name
+LIMIT 10;
+
+--WHERE
+/* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
+-- option 1
+
+SELECT product_id, customer_id
+FROM customer_purchases
+WHERE product_id in (4,9);
+
+-- option 2
+
+/*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
+filtered by vendor IDs between 8 and 10 (inclusive) using either:
+	1.  two conditions using AND
+	2.  one condition using BETWEEN
+*/
+-- option 1
+
+SELECT vendor_id,  product_id, customer_id,
+(quantity*cost_to_customer_per_qty) AS 'price'
+FROM customer_purchases
+WHERE vendor_id >= 8 AND vendor_id <= 10;
+
+-- option 2
+
+SELECT vendor_id, product_id, customer_id,
+(quantity*cost_to_customer_per_qty) AS 'price'
+FROM customer_purchases
+WHERE vendor_id BETWEEN 8 AND 10;
+
+--CASE
+/* 1. Products can be sold by the individual unit or by bulk measures like lbs. or oz. 
+Using the product table, write a query that outputs the product_id and product_name
+columns and add a column called prod_qty_type_condensed that displays the word “unit” 
+if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
+
+SELECT product_id, product_name
+, CASE WHEN product_qty_type = 'unit'
+	THEN 'unit' 
+	ELSE 'bulk'
+	END AS prod_qty_type_condensed
+	
+FROM product;
+
+/* 2. We want to flag all of the different types of pepper products that are sold at the market. 
+add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
+contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
+
+SELECT product_id, product_name
+, CASE WHEN product_qty_type = 'unit'
+	THEN 'unit' 
+	ELSE 'bulk'
+	END AS prod_qty_type_condensed
+, CASE WHEN LOWER(product_name) LIKE LOWER('%pepper%' )
+	THEN 1
+	ELSE 0
+	END AS pepper_flag
+	
+FROM product;
+
+--JOIN
+/* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
+vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
+
+SELECT v.*, vba.*
+FROM vendor as v
+JOIN vendor_booth_assignments AS vba
+ON v.vendor_id = vba.vendor_id
+ORDER BY vendor_name, market_date
