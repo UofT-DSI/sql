83c5843bd0aaedefabd6d3900013523213804fd7
hoseinaliyari <ho3einaliyari@gmail.com>
2024-09-21 23:47:04 -0400

    Submitted homework_5.sql

--- 02_activities/homework/homework_5.sql
+++ 02_activities/homework/homework_5.sql
@@ -1,51 +1,82 @@
--- Cross Join
-/*1. Suppose every vendor in the `vendor_inventory` table had 5 of each of their products to sell to **every** 
-customer on record. How much money would each vendor make per product? 
-Show this by vendor_name and product name, rather than using the IDs.
+-- This I wrote to better understand what is actually happening
 
-HINT: Be sure you select only relevant columns and rows. 
-Remember, CROSS JOIN will explode your table rows, so CROSS JOIN should likely be a subquery. 
-Think a bit about the row counts: how many distinct vendors, product names are there (x)?
-How many customers are there (y). 
-Before your final group by you should have the product of those two queries (x*y).  */
+SELECT  vendor_inventory.vendor_id, vendor.vendor_name, vendor_inventory.product_id, product.product_name, customer.customer_id, 5 * vendor_inventory.original_price
+FROM vendor_inventory
+JOIN vendor ON	vendor_inventory.vendor_id = vendor.vendor_id
+JOIN product ON product.product_id = vendor_inventory.product_id
+CROSS JOIN customer
+ORDER BY vendor_inventory.vendor_id;
 
+-- and this is what is expected, almost the same thing as above but without the extra columns and with the sum aggregation.
 
-
--- INSERT
-/*1.  Create a new table "product_units". 
-This table will contain only products where the `product_qty_type = 'unit'`. 
-It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
-Name the timestamp column `snapshot_timestamp`. */
-
+SELECT  vendor.vendor_name, product.product_name, SUM(5 * vendor_inventory.original_price) as revenue
+FROM vendor_inventory
+JOIN vendor ON	vendor_inventory.vendor_id = vendor.vendor_id
+JOIN product ON product.product_id = vendor_inventory.product_id
+CROSS JOIN customer
+GROUP BY vendor.vendor_name, product.product_name;
 
 
-/*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
-This can be any product you desire (e.g. add another record for Apple Pie). */
+DROP TABLE IF EXISTS product_units;
+CREATE TABLE product_units AS
+SELECT 
+product_id,
+product_name,
+product_size, 
+product_category_id,
+product_qty_type,
+CURRENT_TIMESTAMP AS snapshot_timestamp
+FROM product
+WHERE product_qty_type = 'unit';
 
+INSERT INTO product_units
+VALUES ( 7, 'Apple Pie', '7"', 3, 'unit', CURRENT_TIMESTAMP);
 
-
--- DELETE
-/* 1. Delete the older record for the whatever product you added. 
-
-HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
-
-
-
--- UPDATE
-/* 1.We want to add the current_quantity to the product_units table. 
-First, add a new column, current_quantity to the table using the following syntax.
+DELETE FROM product_units
+WHERE product_id = 7
+AND product_size = '7"';
 
 ALTER TABLE product_units
 ADD current_quantity INT;
 
-Then, using UPDATE, change the current_quantity equal to the last quantity value from the vendor_inventory details.
+-- for finding the last quantity we can find the row with maximum time for each product
 
-HINT: This one is pretty hard. 
-First, determine how to get the "last" quantity per product. 
-Second, coalesce null values to 0 (if you don't have null values, figure out how to rearrange your query so you do.) 
-Third, SET current_quantity = (...your select statement...), remembering that WHERE can only accommodate one column. 
-Finally, make sure you have a WHERE statement to update the right row, 
-	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
-When you have all of these components, you can run the update statement. */
+SELECT pu.product_id, pu.product_name, vi.quantity, vi.market_date
+FROM product_units pu
+LEFT JOIN (
+	SELECT product_id, quantity, market_date
+	FROM vendor_inventory
+	WHERE (product_id, market_date) IN (
+		SELECT product_id, MAX(market_date)
+		FROM vendor_inventory
+		GROUP BY product_id
+		)
+	) vi
+ON pu.product_id = vi.product_id;
 
+-- UPDATE product_units
+-- SET current_quantity = (
+-- 	SELECT COALESCE(vendor_inventory.quantity, 0)
+-- 	FROM vendor_inventory
+-- 	WHERE product_units.product_id = vendor_inventory.product_id
+-- 	AND vendor_inventory.market_date = (
+-- 		SELECT MAX(vendor_inventory.market_date)
+-- 		FROM vendor_inventory
+-- 		WHERE vendor_inventory.product_id = product_units.product_id
+-- 		)
+-- 	)
+
+UPDATE product_units
+SET current_quantity = (
+    SELECT COALESCE(vi.quantity, 0) 
+    FROM vendor_inventory vi
+    WHERE vi.product_id = product_units.product_id
+    AND vi.market_date = (
+        SELECT MAX(vi2.market_date)
+        FROM vendor_inventory vi2
+        WHERE vi2.product_id = product_units.product_id
+    )
+);
+
+-- It seems that my coalesce is not working tho, as I have Null values in my table, needs troubleshooting
 
