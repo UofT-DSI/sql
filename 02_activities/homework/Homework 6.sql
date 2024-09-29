04e3aadb92b9e4ee72d71e4904fd4fff02d376ef
msmacbeth <ionamacbeth@gmail.com>
2024-09-29 06:45:27 -0400

    Add Homework 6 - Reflection

---
+++ 02_activities/homework/Homework 6.sql
@@ -0,0 +1,17 @@
+-- Privacy.  Most of my career is in Information Security.
+-- When I see information that is vulnerable, I have concerns about access, 
+-- retention policies, are we in compliance?  etc.
+-- 
+-- Using the database, or when searching/investigating in general, the first 
+-- thing I always think of is "Am I looking at all the data?  What is mising? 
+-- Do I have all perspectives?"  I find many people aren't even aware how to 
+-- search and manipulate data to get the whole picture.  Is the information
+-- biased?  Am I biased?  These are vital questions. 
+
+-- Without considering these basic questions, you could be leaving room for 
+-- so much imbalance and inequity.  This lack of consideration spills over 
+-- into many other areas.   
+-- 
+-- Run the data again.  Double check.  Triple check sometimes.
+-- 
+-- Those were my biggest takeaways.
