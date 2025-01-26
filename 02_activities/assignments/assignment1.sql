179c04c4ab6095436aa1787dfd6c2027c46bfe0a
minamahdian <mah.mina154@gmail.com>
2025-01-25 21:44:53 -0500

    I finish Section 3.

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -109,7 +109,6 @@ ORDER BY
 --Temp Table
 /* 1. Insert the original vendor table into a temp.new_vendor and then add a 10th vendor: 
 Thomass Superfood Store, a Fresh Focused store, owned by Thomas Rosenthal
-
 HINT: This is two total queries -- first create the table from the original, then insert the new 10th vendor. 
 When inserting the new vendor, you need to appropriately align the columns to be inserted 
 (there are five columns to be inserted, I've given you the details, but not the syntax) 
@@ -117,20 +116,36 @@ When inserting the new vendor, you need to appropriately align the columns to be
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
-
-
+DROP TABLE IF EXISTS temp.new_vendor;
+CREATE TEMPORARY TABLE temp.new_vendor AS
+SELECT * FROM vendor;
+INSERT INTO temp.new_vendor (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+VALUES (10, 'Thomass Superfood Store', 'Fresh Focused', 'Thomas', 'Rosenthal');
 
 -- Date
 /*1. Get the customer_id, month, and year (in separate columns) of every purchase in the customer_purchases table.
-
 HINT: you might need to search for strfrtime modifers sqlite on the web to know what the modifers for month 
 and year are! */
-
-
-
+SELECT 
+    customer_id, 
+    strftime('%m', market_date) AS month, 
+    strftime('%Y', market_date) AS year
+FROM 
+    customer_purchases;
 /* 2. Using the previous query as a base, determine how much money each customer spent in April 2022. 
 Remember that money spent is quantity*cost_to_customer_per_qty. 
 
 HINTS: you will need to AGGREGATE, GROUP BY, and filter...
 but remember, STRFTIME returns a STRING for your WHERE statement!! */
+SELECT 
+    customer_id, 
+    strftime('%m', market_date) AS month, 
+    strftime('%Y', market_date) AS year,
+    SUM(quantity * cost_to_customer_per_qty) AS total_spent
+FROM 
+    customer_purchases
+WHERE 
+    strftime('%m', market_date) = '04' AND strftime('%Y', market_date) = '2022'
+GROUP BY 
+    customer_id;
 
