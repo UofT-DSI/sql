c5a9d48a01b3126db83881b640eafbc5317c0dc8
Veronika <vbrejkaln@gmail.com>
2025-04-19 14:45:32 -0400

    SQL Assignment 1 complete

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -4,21 +4,28 @@
 
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
-
+SELECT * 
+FROM customer;
 
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
-
+SELECT * FROM customer
+ORDER by customer_last_name, customer_first_name
+LIMIT 10;
 
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
 -- option 1
-
+SELECT * 
+FROM customer_purchases
+WHERE product_id = 4 OR product_id = 9;
 
 -- option 2
-
+SELECT *
+FROM customer_purchases
+WHERE product_id IN (4, 9);
 
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
@@ -27,10 +34,14 @@ filtered by vendor IDs between 8 and 10 (inclusive) using either:
 	2.  one condition using BETWEEN
 */
 -- option 1
-
+SELECT *, quantity * cost_to_customer_per_qty AS price
+FROM customer_purchases
+WHERE vendor_id >= 8 AND vendor_id <= 10;
 
 -- option 2
-
+SELECT *, quantity * cost_to_customer_per_qty AS price
+FROM customer_purchases
+WHERE vendor_id BETWEEN 8 AND 10;
 
 
 --CASE
@@ -38,20 +49,40 @@ filtered by vendor IDs between 8 and 10 (inclusive) using either:
 Using the product table, write a query that outputs the product_id and product_name
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
-
+SELECT product_id, product_name,
+    CASE 
+        WHEN product_qty_type = 'unit' 
+		THEN 'unit'
+        ELSE 'bulk'
+    END AS prod_qty_type_condensed
+FROM product;
 
 
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
-
+SELECT product_id, product_name,
+    CASE 
+        WHEN product_qty_type = 'unit' 
+		THEN 'unit'
+        ELSE 'bulk'
+    END AS prod_qty_type_condensed,
+    CASE 
+        WHEN LOWER(product_name) LIKE '%pepper%' 
+		THEN 1
+        ELSE 0
+    END AS pepper_flag
+FROM product;
 
 
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
-
-
+SELECT *
+FROM vendor
+INNER JOIN vendor_booth_assignments
+ON vendor.vendor_id = vendor_booth_assignments.vendor_id
+ORDER BY vendor_name, market_date;
 
 
 /* SECTION 3 */
@@ -59,7 +90,10 @@ vendor_id field they both have in common, and sorts the result by vendor_name, t
 -- AGGREGATE
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
-
+SELECT vendor_id, 
+COUNT(*) AS booth_rentals
+FROM vendor_booth_assignments
+GROUP BY vendor_id;
 
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
@@ -67,7 +101,14 @@ sticker to everyone who has ever spent more than $2000 at the market. Write a qu
 of customers for them to give stickers to, sorted by last name, then first name. 
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
-
+SELECT customer.customer_id, customer_first_name, customer_last_name,
+SUM(quantity * cost_to_customer_per_qty) AS total_spent
+FROM customer
+JOIN customer_purchases
+ON customer.customer_id = customer_purchases.customer_id
+GROUP BY customer.customer_id
+HAVING SUM(quantity * cost_to_customer_per_qty) > 2000
+ORDER BY customer_last_name, customer_first_name;
 
 
 --Temp Table
@@ -81,7 +122,12 @@ When inserting the new vendor, you need to appropriately align the columns to be
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
+CREATE TEMP TABLE new_vendor AS
+SELECT * FROM vendor;
 
+INSERT INTO new_vendor 
+( vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+VALUES (10, 'Thomass Superfood Store', 'Fresh Focused', 'Thomas', 'Rosenthal');
 
 
 -- Date
