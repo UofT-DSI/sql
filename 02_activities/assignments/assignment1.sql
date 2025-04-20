8c8cb25bfebfa3de9724c26e32e637778cb58a27
azharhasan <azharhasan543@gmail.com>
2025-04-20 16:10:24 -0400

    some assignment questions answered

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -5,17 +5,17 @@
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
 
-
+select * from customer;
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
 
-
+select * from customer order by customer_last_name, customer_first_name limit 10;
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
 -- option 1
-
+select * from customer_purchases where product_id=4 or product_id=9;
 
 -- option 2
 
@@ -28,9 +28,10 @@ filtered by vendor IDs between 8 and 10 (inclusive) using either:
 */
 -- option 1
 
-
+select *, (quantity*cost_to_customer_per_qty) as price from customer_purchases where vendor_id>=8 and vendor_id<=10;
 -- option 2
 
+select *, (quantity*cost_to_customer_per_qty) as price from customer_purchases where vendor_id between 8 and 10;
 
 
 --CASE
@@ -39,14 +40,16 @@ Using the product table, write a query that outputs the product_id and product_n
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 
-
+select product_id, product_name, case WHEN product_qty_type='unit' then 'unit'
+									  else 'bulk.' end as prod_qty_type_condensed from product;
 
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 
-
-
+select product_id, product_name, case WHEN product_qty_type='unit' then 'unit'
+									  else 'bulk.' end as prod_qty_type_condensed, case WHEN product_name like '%pepper%' THEN 1
+																						else 0 end as pepper_flag from product
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
