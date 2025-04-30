b590b5c8f9a77f0869d4a1ee82000c43b0d58e2e
CODE, POTATO <68235853+mrpotatocode@users.noreply.github.com>
2025-04-30 13:03:49 +0000

    Update 1nf.sql
    include base table

--- 04_this_cohort/live_code/module_6/1nf.sql
+++ 04_this_cohort/live_code/module_6/1nf.sql
@@ -1,3 +1,16 @@
+drop table if exists temp.skills;
+create temp table if not exists temp.skills
+(
+name TEXT,
+OS TEXT,
+software TEXT,
+supervisor TEXT
+);
+
+insert into temp.skills
+values("A","win","VSCode, MSSQL, RStudio", "Eric Yu"),
+	("Thomas","mac", "Spyder, SQLite, RStudio", "Rohan Alexander");
+
 --1nf
 drop table if exists temp.hold;
 CREATE TABLE temp.hold AS
