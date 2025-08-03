b8c4b1404585bba1eabaebd78ab7db501474c034
CODE, POTATO <t.rose.github@protonmail.com>
2025-08-03 18:27:05 -0400

    remove old code

--- 04_this_cohort/live_code/module_6/penguins_in_python_sql.sql
+++
@@ -1,9 +0,0 @@
-select * from penguins;
-
--- how many penguins were identified each year 
-SELECT DISTINCT year
-,COUNT(*) AS count
-,SUM(COUNT(*)) OVER (ORDER BY year) AS running_total
-
- FROM penguins
-GROUP BY year
