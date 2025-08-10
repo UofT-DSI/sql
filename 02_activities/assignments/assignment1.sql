3fa8eb24ec174ffa66431c3a2c28ea1e0793b84a
Emilia LO <e.lubanska@gmail.com>
2025-08-10 14:25:58 -0400

    temporary tables

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -102,7 +102,16 @@ sticker to everyone who has ever spent more than $2000 at the market. Write a qu
 of customers for them to give stickers to, sorted by last name, then first name. 
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
-SELECT * from customer_purchases
+--SELECT * from customer_purchases
+
+SELECT c.customer_id, c.customer_first_name, c.customer_last_name, SUM(cp.quantity * cp.cost_to_customer_per_qty) AS total_spent
+FROM customer as c
+JOIN customer_purchases as cp ON c.customer_id = cp.customer_id
+GROUP BY
+    c.customer_id,
+    c.customer_first_name,
+    c.customer_last_name
+HAVING SUM(cp.quantity * cp.cost_to_customer_per_qty) > 2000
 
 
 --Temp Table
@@ -116,8 +125,17 @@ When inserting the new vendor, you need to appropriately align the columns to be
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
+CREATE TEMPORARY TABLE new_vendor AS
+SELECT * FROM vendor
 
+--drop TABLE new_vendor
 
+--SELECT * from new_vendor
+
+INSERT INTO new_vendor (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+VALUES (10, 'Thomass Superfood Store', 'Fresh Focused', 'Thomas', 'Rosenthal')
+
+SELECT * from new_vendor
 
 -- Date
 /*1. Get the customer_id, month, and year (in separate columns) of every purchase in the customer_purchases table.
