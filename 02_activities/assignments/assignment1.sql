e0b251b8d7f32c47eccb12c1e8f596a1a9f0330e
Emilia LO <e.lubanska@gmail.com>
2025-08-10 11:38:57 -0400

    update

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -31,11 +31,11 @@ filtered by vendor IDs between 8 and 10 (inclusive) using either:
 	2.  one condition using BETWEEN
 */
 -- option 1
-
-
+SELECT *,  (quantity * cost_to_customer_per_qty) as price from customer_purchases
+where vendor_id >= 8 and vendor_id <= 10
 -- option 2
-
-
+SELECT *,  (quantity * cost_to_customer_per_qty) as price from customer_purchases
+where vendor_id BETWEEN 8 and 10
 
 --CASE
 /* 1. Products can be sold by the individual unit or by bulk measures like lbs. or oz. 
@@ -43,20 +43,42 @@ Using the product table, write a query that outputs the product_id and product_n
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 
+--SElect * from product
+
+Select product_id, product_name, product_qty_type, 
+	CASE
+		when product_qty_type = 'unit' THEN 'unit'
+		else 'bulk'
+	End as prod_qty_type_condensed
+From product
 
 
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 
-
+Select product_id, product_name, product_qty_type, 
+	CASE
+		when product_qty_type = 'unit' THEN 'unit'
+		else 'bulk'
+	End as prod_qty_type_condensed,
+	Case 
+		when product_name like '%pepper%' then 1
+		Else 0
+	End pepper_flag
+From product
 
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
+--SELECT * FROM vendor
+--SELECT * FROM vendor_booth_assignments
 
-
-
+SELECT * from vendor as v
+	inner JOIN vendor_booth_assignments as va on v.vendor_id = va.vendor_id
+order by
+	v.vendor_name ASC,
+	va.market_date ASC
 
 /* SECTION 3 */
 
