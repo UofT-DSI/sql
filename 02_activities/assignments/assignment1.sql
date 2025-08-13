4db53b803ae1ae3080013a9bdeccf835c84e5be5
Christina Lamparter <cl.lamparter@gmail.com>
2025-08-13 19:54:03 -0400

    Uploading assignment as .sql

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -5,21 +5,31 @@
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
 
+SELECT * 
+FROM customer;
 
 
-/* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
+/* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table 
 sorted by customer_last_name, then customer_first_ name. */
 
+SELECT * 
+FROM customer
+ORDER BY customer_last_name ASC, customer_first_name ASC
+LIMIT 10;
 
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
 -- option 1
 
+SELECT * 
+FROM customer_purchases
+WHERE product_id IN (4,9);
 
 -- option 2
 
-
+SELECT * FROM customer_purchases
+WHERE product_id = 4 OR product_id = 9;
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
 filtered by vendor IDs between 8 and 10 (inclusive) using either:
@@ -28,10 +38,15 @@ filtered by vendor IDs between 8 and 10 (inclusive) using either:
 */
 -- option 1
 
+SELECT *, quantity * cost_to_customer_per_qty AS 'price'
+FROM customer_purchases
+WHERE vendor_id BETWEEN 8 AND 10;
 
 -- option 2
 
-
+SELECT *, quantity * cost_to_customer_per_qty AS 'price'
+FROM customer_purchases
+WHERE vendor_id >= 8 AND vendor_id <= 10;
 
 --CASE
 /* 1. Products can be sold by the individual unit or by bulk measures like lbs. or oz. 
@@ -39,20 +54,35 @@ Using the product table, write a query that outputs the product_id and product_n
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 
-
+SELECT product_id, product_name,
+CASE WHEN product_qty_type='unit' THEN 'unit'
+	ELSE 'bulk'
+END AS prod_qty_type_condensed
+FROM product;
 
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 
+SELECT product_id, product_name,
+CASE WHEN product_qty_type='unit' THEN 'unit'
+	ELSE 'bulk'
+END AS prod_qty_type_condensed,
 
+CASE WHEN product_name LIKE '%pepper%' THEN 1
+	ELSE 0
+END AS pepper_flag
+FROM product;
 
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
 
-
-
+SELECT v.vendor_id, vendor_name, market_date
+FROM vendor AS v
+	INNER JOIN vendor_booth_assignments AS vba
+		ON v.vendor_id = vba.vendor_id
+ORDER BY vendor_name ASC, market_date ASC;
 
 /* SECTION 3 */
 
@@ -60,7 +90,10 @@ vendor_id field they both have in common, and sorts the result by vendor_name, t
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
 
-
+SELECT vba.vendor_id,
+	COUNT(vendor_id) AS tot_rentals
+FROM vendor_booth_assignments AS vba
+GROUP BY vendor_id;
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
 sticker to everyone who has ever spent more than $2000 at the market. Write a query that generates a list 
@@ -68,7 +101,13 @@ of customers for them to give stickers to, sorted by last name, then first name.
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
 
-
+SELECT cp.customer_id, customer_first_name, customer_last_name, ROUND(SUM(cp.quantity*cp.cost_to_customer_per_qty),2) AS tot_spent
+FROM customer_purchases AS cp
+	INNER JOIN customer AS c
+		ON cp.customer_id = c.customer_id
+GROUP BY cp.customer_id
+HAVING tot_spent>2000
+ORDER BY customer_last_name ASC, customer_first_name ASC;
 
 --Temp Table
 /* 1. Insert the original vendor table into a temp.new_vendor and then add a 10th vendor: 
@@ -82,6 +121,14 @@ When inserting the new vendor, you need to appropriately align the columns to be
 VALUES(col1,col2,col3,col4,col5) 
 */
 
+DROP TABLE IF EXISTS temp.new_vendor;
+
+CREATE TABLE temp.new_vendor AS
+SELECT *
+FROM vendor;
+
+INSERT INTO temp.new_vendor (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+VALUES (10, 'Thomass Superfood Store', 'A Fresh Focused Store', 'Thomas', 'Rosenthal');
 
 
 -- Date
