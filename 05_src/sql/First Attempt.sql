98a7fb37ce20255688577a92cf3a9c0678c71797
Armita kh <kharmandara@users.noreply.github.com>
2025-11-15 15:27:12 -0500

    practices_during_class

---
+++ 05_src/sql/First Attempt.sql
@@ -0,0 +1,29 @@
+/* MODULE 2 */
+/* SELECT */
+
+
+/* 1. Select everything in the customer table */
+SELECT  * FROM customer;
+
+/* 2. Use sql as a calculator */
+
+SELECT 1+1 as [2], pi() as [pi];
+
+/* 3. Add order by and limit clauses */
+
+SELECT * FROM customer 
+ORDER BY customer_last_name DESC --z>a
+LIMIT 5; -- only 5 row
+
+/* 4. Select multiple specific columns */
+SELECT product_name, product_size, product_qty_type
+FROM product;
+
+
+/* 5. Add a static value in a column */
+SELECT '2025' as [this_year],
+vendor_name,
+vendor_type
+FROM vendor;
+
+SELECT * FROM vendor
