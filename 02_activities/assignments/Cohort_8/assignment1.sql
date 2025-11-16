ec3ac99bde558388281d6b37e4a70e15580b89a0
Saqib Syed <saqibssyed@gmail.com>
2025-11-16 16:08:52 -0500

    Added assignment1.sql file to assignment-one branch, without DATE section completed

--- 02_activities/assignments/Cohort_8/assignment1.sql
+++ 02_activities/assignments/Cohort_8/assignment1.sql
@@ -4,18 +4,34 @@
 
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
-
+SELECT * 
+FROM customer;
 
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
-
+SELECT *
+FROM customer 
+ORDER BY customer_last_name ASC, customer_first_name ASC
+LIMIT 10;
 
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
+SELECT c.customer_id,
+	c.customer_first_name,
+	c.customer_last_name,
+	cp.product_id,
+	cp.market_date,
+	cp.quantity,
+	p.product_name
 
-
+FROM customer_purchases cp
+INNER JOIN customer c
+	ON cp.customer_id = c.customer_id
+INNER JOIN product p
+	ON cp.product_id = p.product_id
+WHERE cp.product_id = 4 OR cp.product_id = 9;
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
 filtered by customer IDs between 8 and 10 (inclusive) using either:
@@ -23,10 +39,38 @@ filtered by customer IDs between 8 and 10 (inclusive) using either:
 	2.  one condition using BETWEEN
 */
 -- option 1
+SELECT c.customer_id,
+	c.customer_first_name,
+	c.customer_last_name,
+	cp.product_id,
+	cp.market_date,
+	cp.quantity,
+	p.product_name,
+	(cp.quantity*cp.cost_to_customer_per_qty) as price
 
+FROM customer_purchases cp
+JOIN customer c
+	ON cp.customer_id = c.customer_id
+JOIN product p
+	ON cp.product_id = p.product_id
+WHERE c.customer_id >= 8 AND c.customer_id <= 10;
 
 -- option 2
+SELECT c.customer_id,
+	c.customer_first_name,
+	c.customer_last_name,
+	cp.product_id,
+	cp.market_date,
+	cp.quantity,
+	p.product_name,
+	(cp.quantity*cp.cost_to_customer_per_qty) as price
 
+FROM customer_purchases cp
+JOIN customer c
+	ON cp.customer_id = c.customer_id
+JOIN product p
+	ON cp.product_id = p.product_id
+WHERE c.customer_id BETWEEN 8 AND 10;
 
 
 --CASE
@@ -34,20 +78,41 @@ filtered by customer IDs between 8 and 10 (inclusive) using either:
 Using the product table, write a query that outputs the product_id and product_name
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
-
+SELECT product_id,
+	product_name,
+	CASE 
+		WHEN product_qty_type = 'unit' THEN 'unit'
+		ELSE 'bulk'
+	END AS prod_qty_type_condensed
+FROM product;
 
 
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
-
+SELECT product_id,
+	product_name,
+	CASE 
+		WHEN product_qty_type = 'unit' THEN 'unit'
+		ELSE 'bulk'
+	END AS prod_qty_type_condensed,
+	CASE
+		WHEN product_name LIKE '%pepper%' THEN '1'
+		ELSE '0'
+	END AS pepper_flag
+FROM product;
 
 
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
-
-
+SELECT 
+	v.vendor_name,
+	vba.market_date
+FROM vendor v
+INNER JOIN vendor_booth_assignments vba
+	ON vba.vendor_id = v.vendor_id
+ORDER BY v.vendor_name, vba.market_date;
 
 
 /* SECTION 3 */
@@ -55,7 +120,14 @@ vendor_id field they both have in common, and sorts the result by vendor_name, t
 -- AGGREGATE
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
-
+SELECT vba.vendor_id,
+	v.vendor_name,
+	COUNT(*) as vendor_booth_assignment_count
+FROM vendor v 
+INNER JOIN vendor_booth_assignments vba
+		ON vba.vendor_id = v.vendor_id
+GROUP BY vba.vendor_id; 
+		
 
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
@@ -63,7 +135,16 @@ sticker to everyone who has ever spent more than $2000 at the market. Write a qu
 of customers for them to give stickers to, sorted by last name, then first name. 
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
+SELECT c.customer_id,
+	c.customer_first_name,
+	c.customer_last_name,
+	SUM(cp.quantity*cp.cost_to_customer_per_qty) as total_spend
 
+FROM customer_purchases cp
+INNER JOIN customer c
+	ON cp.customer_id = c.customer_id
+GROUP BY cp.customer_id HAVING total_spend > 2000
+ORDER BY customer_last_name, customer_first_name;
 
 
 --Temp Table
@@ -77,7 +158,14 @@ When inserting the new vendor, you need to appropriately align the columns to be
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
-
+--Create the temp table 
+DROP TABLE IF EXISTS temp.new_vendor;
+CREATE TEMPORARY TABLE temp.new_vendor AS
+SELECT *
+FROM vendor;
+--Insert the new column with values
+INSERT INTO temp.new_vendor (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+VALUES (10, 'Thomass Superfood Store', 'a Fresh Focused Store', 'Thomas', 'Rosenthal');
 
 
 -- Date
