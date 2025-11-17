1c2ad29f318932fb040648c91012cab81c0b75a7
Emre Ozkan <mr.ozkan@outlook.com>
2025-11-16 19:46:26 -0500

    Emre Ozkan Assignment 1

--- 02_activities/assignments/Cohort_8/assignment1.sql
+++ 02_activities/assignments/Cohort_8/assignment1.sql
@@ -1,21 +1,23 @@
 /* ASSIGNMENT 1 */
 /* SECTION 2 */
-
+/*Emre OZKAN github: https://github.com/mrozkan/sql/tree/assignment-one */
 
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
 
-
+SELECT * FROM customer;
 
 /* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
 sorted by customer_last_name, then customer_first_ name. */
+
+SELECT * FROM customer ORDER BY customer_last_name, customer_first_name LIMIT 10;
 
 
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
 
-
+SELECT * FROM customer_purchases WHERE product_id IN (4, 9);
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
 filtered by customer IDs between 8 and 10 (inclusive) using either:
@@ -23,9 +25,14 @@ filtered by customer IDs between 8 and 10 (inclusive) using either:
 	2.  one condition using BETWEEN
 */
 -- option 1
-
+SELECT *, quantity * cost_to_customer_per_qty AS price 
+FROM customer_purchases 
+WHERE customer_id >= 8 AND customer_id <= 10;
 
 -- option 2
+SELECT *, quantity * cost_to_customer_per_qty AS price 
+FROM customer_purchases 
+WHERE customer_id BETWEEN 8 AND 10;
 
 
 
@@ -35,17 +42,41 @@ Using the product table, write a query that outputs the product_id and product_n
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 
-
+SELECT 
+	product_id,
+	product_name,
+	CASE
+		WHEN product_qty_type = "unit" THEN "unit"
+		ELSE "bulk"
+	END AS prod_qty_type_condensed
+FROM product;
 
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 
+SELECT 
+	product_id,
+	product_name,
+	CASE
+		WHEN product_qty_type = "unit" THEN "unit"
+		ELSE "bulk"
+	END AS prod_qty_type_condensed,
+	CASE
+		WHEN LOWER(product_name) LIKE "%pepper%" THEN 1
+		ELSE 0
+	END AS pepper_flag
+FROM product;
 
 
 --JOIN
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
+
+SELECT *
+FROM vendor
+INNER JOIN vendor_booth_assignments ON vendor.vendor_id = vendor_booth_assignments.vendor_id
+ORDER BY vendor_name, market_date;
 
 
 
@@ -56,6 +87,11 @@ vendor_id field they both have in common, and sorts the result by vendor_name, t
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
 
+SELECT 
+	vendor_id,
+	COUNT(*) AS booth_rental_count
+FROM vendor_booth_assignments
+GROUP BY vendor_id;
 
 
 /* 2. The Farmer’s Market Customer Appreciation Committee wants to give a bumper 
@@ -63,6 +99,17 @@ sticker to everyone who has ever spent more than $2000 at the market. Write a qu
 of customers for them to give stickers to, sorted by last name, then first name. 
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
+
+SELECT 
+	c.customer_id,
+	c.customer_first_name,
+	c.customer_last_name,
+	SUM(cp.quantity * cp.cost_to_customer_per_qty) AS total_spent
+FROM customer c
+INNER JOIN customer_purchases cp ON c.customer_id = cp.customer_id
+GROUP BY c.customer_id, c.customer_first_name, c.customer_last_name
+HAVING total_spent > 2000
+ORDER BY c.customer_last_name, c.customer_first_name;
 
 
 
@@ -77,6 +124,11 @@ When inserting the new vendor, you need to appropriately align the columns to be
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
+
+CREATE TABLE temp.new_vendor AS SELECT * FROM vendor;
+
+INSERT INTO temp.new_vendor 
+VALUES (10, 'Thomass Superfood Store', 'Fresh Focused', 'Thomas Rosenthal');
 
 
 
