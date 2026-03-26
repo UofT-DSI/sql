3c3808b069f4e081396b6d807ccc27f7bbf67fa3
Evelyn <evelynlin1224@gmail.com>
2026-03-25 21:24:45 -0400

    finish Assignment 1

---
+++ 04_this_cohort/Mar26_Live code.sql
@@ -0,0 +1,46 @@
+/* MODULE 2 */
+/* LEFT  JOIN */
+
+
+/* 1. There are products that have been bought
+... but are there products that have not been bought? 
+Use a LEFT JOIN to find out*/
+SELECT DISTINCT
+p.product_id
+,cp.product_id as [cp.product_id]
+,product_name
+
+FROM product as p
+LEFT JOIN customer_purchases as cp
+	ON p.product_id =  cp.product_id
+	
+/* 2. Directions of LEFT JOINs matter ...*/
+
+FROM customer_purchases as cp
+LEFT JOIN product as p
+	ON p.product_id = cp.product_id;
+
+
+/* 3. As do which values you filter on ... */
+SELECT DISTINCT
+p.product_id
+,cp.product_id
+
+
+
+/* 4. Without using a RIGHT JOIN, make this query return the RIGHT JOIN result set
+...**Hint, flip the order of the joins** ...
+
+SELECT *
+
+FROM product_category AS pc
+LEFT JOIN product AS p
+	ON pc.product_category_id = p.product_category_id
+	ORDER by pc.product_category_id
+
+...Note how the row count changed from 24 to 23
+*/	
+
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------
